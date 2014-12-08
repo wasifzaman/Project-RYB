2b89defbd805f1b2cc59f1265d7c05e263296f17
wasifzaman <zelvenstormrage@gmail.com>
2014-12-08 02:11:26 -0500

    created classes
    Widget_
    Textbox(Widget_)
    Button_(Widget_)
    Multilne_textbox(Textbox)
    
    do:
    Date_entry(Textbox)
    Coin_entry(Textbox)
    Single_select(Widget_)

---
+++ widgets/class_textbox_widget.py
@@ -0,0 +1,35 @@
+'''
+'''
+from class_widget import Widget_
+from tkinter import Frame, Label, Entry, StringVar, LEFT
+
+class Textbox(Widget_):
+
+	def __init__(self, parent_frame, x, y):
+		Widget_.__init__(self, parent_frame, x, y)
+		self.label_width = 20
+		self.entry_width = 30
+		self.stringvar = StringVar()
+		self.label = Label(self.widget_frame, width=self.label_width)
+		self.entry = Entry(self.widget_frame, width=self.entry_width, textvariable=self.stringvar)
+		self.label.pack(side=LEFT)
+		self.entry.pack(side=LEFT)
+		self.label_bg, self.label_fg, self.label_hover_bg, self.label_hover_fg = None, None, None, None
+		self.entry_bg, self.entry_fg, self.entry_hover_bg, self.entry_hover_fg = None, None, None, None
+
+	def get_info(self):
+		return self.label.cget('text'), self.entry.get()
+
+	def set_text_field(self, label=False, entry=False):
+		if label:
+			self.label.config(text=label)
+		if entry:
+			self.stringvar.set(entry)
+
+	def set_setting(self):
+		return
+
+	def set_input_restriction(self, string):
+		return
+
+	pass
