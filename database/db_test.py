3be3e7fd33eabe7a1bd0421f5d7feb4016f1beec
wasifzaman <zelvenstormrage@gmail.com>
2014-12-24 16:58:23 -0500

    sql

--- database/db_test.py
+++ database/db_test.py
@@ -1,95 +1,58 @@
 import sqlite3
 import datetime
-
+import shutil
 
 
 class Database_editor:
 
-	def create_database(self, db_filepath):
+	def create_open_database(self, db_filepath, db_templ_filepath=False):
+		if db_templ_filepath:
+			shutil.copy(db_templ_filepath, db_filepath)
 		self.conn = sqlite3.connect(db_filepath)
 		self.cur = self.conn.cursor()
-		self.cur.execute('''
-			CREATE TABLE student_info
-				(barcode text unique, first_name text, last_name text, chinese_name text, dob text, age real, card_printed integer,
-					address text, city text, state text, zip integer, email text,
-					parent_name text, pick_up_person text, home_phone integer, cell_phone1 integer, cell_phone2 integer,
-					notes text)
-			''')
-		self.cur.execute('''
-			CREATE TABLE attendance
-				(barcode text unique, check_in_date text, check_in_time text, class_time text, scan_type text)
-			''')
-		self.cur.execute('''
-			CREATE TABLE payment
-				(barcode text unique, tuition_pay_date text, total_paid real, amount_owed real, service_type text, classes_awarded integer, classes_remaining integer, regular_class_time text)
-			''')
-		self.conn.commit()
-		self.conn.close()
 
 	def open_database(self, db_filepath):
 		self.conn = sqlite3.connect(db_filepath)
 		self.cur = self.conn.cursor()
 
-	def add_student(self, data):
-		columns = [column for column in self.cur.execute('PRAGMA table_info(student_info)')]
-		ordered_columns = tuple([key[1] for key in columns])
-		filled_values = tuple([data[key] for key in ordered_columns])
-		script = 'INSERT INTO student_info ' + str(ordered_columns) + ' values ' + str(filled_values)
-		self.cur.execute(script)
-		self.conn.commit()
+	def add_student(self, data_table):
+		print([row[1] for row in self.cur.execute('PRAGMA table_info(student_info)')])
+
+		#[row for row in self.cur.execute('''
+		#	SELECT table_name
+		#	FROM USER_TAB_COLUMNS WHERE table_name=?''', ('student_info', ))]
+		return
 
 	def get_timeslot(self):
 		return
 
 	def scan_student(self, barcode, time=False, scan_type='Barcode'):
-		scan = (str(barcode, datetime.now().date, datetime.now().time, datetime.now().time, scan_type), )
-		self.cur.execute('INSERT INTO attendance VALUES symbol=?', scan)
-
-	def search_database(self, barcode, table):
-		barcode = (barcode, )
-		script = 'SELECT * FROM ' + table + ' WHERE barcode=?'
-		x.cur.execute(script, barcode)
+		return
 		
-		return x.cur.fetchone()
-
+	def search_database(self, barcode, table):
+		return
 
 
 
 x = Database_editor()
 
+
 #x.create_database('test.db')
-data = {'barcode': 'asdw2', 'first_name': 'a', 'last_name': 'b', 'chinese_name': 'c', 'dob': 'd', 'age': 'e', 'card_printed': 'f',
-		'address': 'a', 'city': 'b', 'state': 'c', 'zip': 'd', 'email': 'e',
-		'parent_name': 'a', 'pick_up_person': 'b', 'home_phone': 'c', 'cell_phone1': 'd', 'cell_phone2': 'e',
-		'notes': ''}
+data = {}
 
-x.open_database('test.db')
-#x.add_student(data)
+#x.create_open_database('test2.db', 'student_db_template.db')
+x.create_open_database('test2.db')
 
-#barcode = (1234, )
-#x.cur.execute('SELECT * FROM general_info WHERE barcode=?', barcode)
-#row = x.cur.fetchone()
-#print(row)
-	
-all_ = x.search_database('asdw2', 'student_info')
-print(all_)
+x.add_student(data)
 
+x.conn.close()
 
+#conn = sqlite3.connect('test2.db')
 
+#c = conn.cursor()
 
-
-
-
-
-
-
-
-
-
-
-conn = sqlite3.connect('test2.db')
-
-c = conn.cursor()
+#r = [row for row in c.execute('SELECT * FROM student_info')]
+#print(r)
 
 #c.execute('''
 #		CREATE TABLE student_info
@@ -123,4 +86,4 @@ c = conn.cursor()
 
 #print(r)
 
-conn.close()
+#conn.close()
